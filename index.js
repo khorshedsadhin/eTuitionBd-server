7a3f28bcaa46d9d28ffd842b26bc4e947a9cf271
khorshedsadhin <khorshedsadhin81@gmail.com>
2025-12-22 11:06:56 +0600

    added api for saving users in database

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 
+/*
+  ? TODOS:
+  ? 1. jwt, axios, role base middleware, dashboard role based, student, tutor, admin, then all the mainlayout design
+*/
+
 const app = express();
 
 // middleware
@@ -25,6 +30,38 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 });
 async function run() {
 	try {
+    const db = client.db("eTuitionBD");
+    const usersCollection = db.collection("users");
+
+    //* save user to database
+    app.post('/user', async(req, res) => {
+      const userData = req.body;
+      userData.created_at = new Date().toISOString();
+      userData.last_loggedIn = new Date().toISOString();
+      
+      const query = { email: userData.email };
+      const alreadyExists = await usersCollection.findOne(query);
+      console.log('user already exists ----> ', !!alreadyExists);
+
+      if(alreadyExists) {
+        console.log('updating user info...');
+        const result = usersCollection.updateOne(query, { $set: { last_loggedIn: new Date().toISOString() } });
+        return res.send(result);
+      }
+
+      console.log("saving new user info...");
+      const result = await usersCollection.insertOne(userData);
+      
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log(
